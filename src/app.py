c2ea04c574bbb4af60c342b3d54e78e457f793ed
Torstein Holmelid <torsteinholmelid@hotmail.com>
2026-05-12 20:52:07 +0200

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1158,7 +1158,7 @@ if results:
         </svg>
 
         <div class="callout">
-          <b>Interpretasjon:</b> du ligg svært høgt i forhold til snittet for alderen din.
+          <b>Interpretasjon:</b> You are performing very well compared to the average for your age.
         </div>
 
         <div class="legend-col">
