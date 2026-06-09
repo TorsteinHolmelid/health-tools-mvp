06bbb15a91e6fc43f787fce556cbac4f7eaf529b
Torstein Holmelid <torsteinholmelid@hotmail.com>
2026-06-09 10:03:34 +0200

    Update app.py

--- src/app.py
+++ src/app.py
@@ -4259,7 +4259,7 @@ else:
         
         # 2. Generer PDF og vis rapporten på skjermen
         try:
-            pdf_bytes = create_pdf_bytes_ultimate(report)
+        
             
             # Siden alt gikk bra, viser vi resultat-boksen og nedlastingsknappen her:
             with st.container(border=True):
